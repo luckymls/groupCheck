1dc0005c05740a995b335e5efb7a46edd3c89ed5
luckymls <melis.luca2014@gmail.com>
2019-11-02 00:37:09 +0100

    Create include.php

---
+++ include.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'bot.php'; //variabili e funzioni
+include 'tabs.php';
+include 'impostazioni.php';
+include 'impostazioni_explicit.php';
+include 'blocco.php'; //blocco utenti & bot
+include 'manage.php';
+include 'mutato.php';
+include 'warn.php';
+include 'menu.php';
+include 'cancellazione.php';
+include 'traduzione.php';
+
+?>
